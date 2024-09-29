e518981dad38f6262876daf84f956864a4ad08e7
GustavoCandido <88414496+gut1Rangell@users.noreply.github.com>
2024-09-29 19:55:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const body = document.body;
+const loginBox = document.querySelector(".login-box");
+const h1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
+const inputs = document.querySelectorAll("input");
+const loginButton = document.getElementById("login-button");
+const themeToggleButton = document.getElementById("theme-toggle");
+
+function switchTheme() {
+    const isDarkMode = body.classList.toggle("dark-mode");
+    loginBox.classList.toggle("dark-mode");
+    h1.classList.toggle("dark-mode");
+    h2.classList.toggle("dark-mode");
+    
+    inputs.forEach(input => {
+        input.classList.toggle("dark-mode");
+    });
+
+    loginButton.classList.toggle("dark-mode");
+
+    // Armazenar a preferência do tema no localStorage
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+}
+
+// Carregar o tema preferido ao iniciar
+function loadTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        switchTheme();
+    }
+}
+
+// Evento para alternar o tema
+themeToggleButton.addEventListener("change", switchTheme);
+
+// Carregar o tema ao iniciar
+loadTheme();
